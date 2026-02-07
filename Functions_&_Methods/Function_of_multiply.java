c07af64fb54509a89be025e52852e0b4d891187a
Shubham yadav <shubhamyadavgpu@gmail.com>
2026-02-07 21:34:05 +0530

    Function of Multiply

---
+++ Functions_&_Methods/Function_of_multiply.java
@@ -0,0 +1,12 @@
+public class Function_of_multiply {
+    public static int Multiply (int m, int n){
+        int mul = m * n ;
+        return mul;
+    }
+    public static void main(String args[]){
+        int a = 5 ;
+        int b = 8 ;
+        System.out.println("Multiply = "+Multiply(a,b));
+        System.out.println("Multiply = "+Multiply(10,23));
+    }
+}
