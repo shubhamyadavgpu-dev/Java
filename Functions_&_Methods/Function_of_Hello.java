9ead071d46347518cf2e1ad9e105ae020e4ca0d3
Shubham yadav <shubhamyadavgpu@gmail.com>
2026-02-17 23:57:09 +0530

    Delete Functions_&_Methods directory

--- Functions_&_Methods/Function_of_Hello.java
+++
@@ -1,8 +0,0 @@
-public class Function_of_Hello {
-    public static void hello (){
-        System.out.println("Hello World!");
-    }
-    public static void main (String args[]){
-       hello();
-    }
-}
